45e6b3e8c9f0c966b5b4c198b1a765970b1fb76d
Thomas Altree <altree.thomas@googlemail.com>
2024-12-22 18:56:24 +0000

    Add internet gateway to public subnet

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -12,7 +12,35 @@ resource "aws_subnet" "tutorial" {
   vpc_id     = aws_vpc.tutorial.id
   cidr_block = "10.0.0.0/24"
 
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-tutorial"
+  }
+}
+
+resource "aws_internet_gateway" "tutorial" {
+  vpc_id = aws_vpc.tutorial.id
+
   tags = {
     Name = "tutorial"
   }
+}
+
+resource "aws_route_table" "tutorial" {
+  vpc_id = aws_vpc.tutorial.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tutorial.id
+  }
+
+  tags = {
+    Name = "public-tutorial"
+  }
+}
+
+resource "aws_route_table_association" "tutorial" {
+  subnet_id      = aws_subnet.tutorial.id
+  route_table_id = aws_route_table.tutorial.id
 }
